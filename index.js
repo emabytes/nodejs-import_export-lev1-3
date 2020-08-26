726cbb8a9773f2a8589f1ff963bd517a550d6322
emabytes <emadavor22@gmail.com>
2020-08-27 00:08:49 +0200

    second

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
 const fn = require("./functions")
 
 //1_3
-
-const test = fn.calculate(2, 2, "+")
+const test = fn.calculate(256, 2, "+")
 console.log(test)
-
-
-//3_1
-
-fn.appendToFile(45)
-fn.appendToFile(5)
-fn.appendToFile(35)
-
-
-//3_2
-
-fn.checkCreateDelete()
